a72925359c09b0141bbcfa96081b04173cff296f
NikithaDevasoth <devasothnikitha09@gmail.com>
2025-05-29 15:21:12 +0530

    library changes

--- Frontend/dashboard/src/vendorDashboard/components/forms/AddFirm.jsx
+++ Frontend/dashboard/src/vendorDashboard/components/forms/AddFirm.jsx
@@ -1,5 +1,6 @@
 
-import React, {useState} from 'react'
+import  {useState} from 'react'
+import React from 'react';
 import { API_URL } from '../../data/apiPath';
 import { ThreeCircles } from 'react-loader-spinner';
 
